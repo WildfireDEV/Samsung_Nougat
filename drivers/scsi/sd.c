b9e3ca9446df710fc783bf91ee50ef2f13190117
HRTKernel <support@hacker-kernel.com>
2015-05-31 16:52:28 +0200

    SCSI: Forbid cache flush for write protected devices
    Conflicts:
    	fmp_hmac.bin
    
    Commit by stefant234
    https://github.com/stefant234/SimplKernel-LL-G925F/commit/2aeef5f3aa7242cbffb11d2601855f1a8a26be6c

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -3290,8 +3290,8 @@ static int sd_suspend(struct device *dev)
 	struct scsi_disk *sdkp = scsi_disk_get_from_dev(dev);
 	int ret = 0;
 
-	if (!sdkp)	/* E.g.: runtime suspend following sd_remove() */
-		return 0;
+	if (!sdkp)
+		return 0;	/* this can happen */
 
 	if (sdkp->WCE) {
 		sd_printk(KERN_NOTICE, sdkp, "Synchronizing SCSI cache\n");
